f7735a14730d5481ce049f7a78b597a45bedf96b
Albert <albert.canellas@estudiants.urv.cat>
2017-03-15 12:16:07 +0100

    daf

---
+++ src/Aplicacio/Aplicacio.java
@@ -0,0 +1,14 @@
+package Aplicacio;
+
+public class Aplicacio {
+
+	public Aplicacio() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
