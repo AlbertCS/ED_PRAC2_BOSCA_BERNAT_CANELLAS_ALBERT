0dbc0b1320781e6e0b35e8b423fce821ad861b67
Bernat Bosca <bernat.bosca@urv.cat>
2017-04-04 01:10:11 +0200

    Canvios +Multillista a la mitad

--- src/Dades/Llistes/Multillista.java
+++ src/Dades/Llistes/Multillista.java
@@ -1,8 +1,6 @@
-package Dades.Base;
-import Dades.Llistes.LlistaDinamica;
-import Dades.Llistes.LlistaEstatica;
-import Dades.Llistes.TADLlistaGenerica;
+package Dades.Llistes;
 import Exceptions.*;
+import Dades.Base.*;
 
 public class Multillista<T extends Comparable<T>> {
 	private TADLlistaGenerica<T> llistaAssig;
@@ -26,6 +24,35 @@ public class Multillista<T extends Comparable<T>> {
 				this.llistaAlum.afegirElement(llistaAlum.consultarPosicio(i));
 		} catch (LlistaPlena|LlistaBuida e){
 			System.out.println("Aquesta circunstancia no deuria de passar mai.");
+		}
+	}
+	
+	public void afegirMatricula (Integer assig, String alum) {
+		int i=0;
+		Matricula newMat=new Matricula(assig, alum);
+		Assignatura assigAux=null;
+		Alumne aluAux=null;
+		Matricula matAux=null;
+		try {
+			assigAux=(Assignatura) llistaAssig.consultarPosicio(i);
+			while(assig!=assigAux.getCodiAssig()) {
+				i++;
+				assigAux=(Assignatura) llistaAssig.consultarPosicio(i);
+			}
+			matAux=assigAux.getMatric();
+			if(matAux==null) assigAux.setMatric(newMat);
+			else {
+				while(matAux.getSeguentAssig()!=null) matAux=matAux.getSeguentAssig();
+				matAux.setSeguentAssig(newMat);
+			}
+			aluAux=(Alumne) llistaAssig.consultarPosicio(i);
+			while(alum!=aluAux.getCodiAlum()) {
+				i++;
+				aluAux=(Alumne) llistaAssig.consultarPosicio(i);
+			}
+			aluAux.setMatric(newMat);
+		} catch (LlistaBuida e) {
+			e.printStackTrace();
 		}
 	}
 }
